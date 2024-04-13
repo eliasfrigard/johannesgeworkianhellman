5aea6f831fd79c130edc397e19677c448de3cca5
Elias Frigård <elias.frigard@tvtools.fi>
2024-04-13 21:00:52 +0300

    fix no-click header

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['images.ctfassets.net'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+    ]
   },
 }
 
