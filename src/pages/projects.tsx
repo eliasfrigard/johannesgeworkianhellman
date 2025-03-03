00a500fcbd6873134f90c8f5a4e39a6dce6682d5
Elias Frigård <elias.frigard@mediatailor.fi>
2025-03-03 20:30:10 +0200

    fix projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,15 +1,11 @@
 import Image from 'next/image'
-import Layout from "@/layouts/default"
+import Layout from '@/layouts/default'
 import Band from '@/components/Band'
 import Container from '@/components/Container'
 import { createClient } from 'contentful'
 import { getPlaiceholder } from 'plaiceholder'
 
-
-import {
-  Hero,
-  getImageBuffer
-} from 'eliasfrigard-reusable-components/dist/app'
+import { Hero, getImageBuffer } from 'eliasfrigard-reusable-components/dist/app'
 
 import TextLayout from '@/components/TextLayout'
 
@@ -22,7 +18,7 @@ export async function getStaticProps() {
   const pageRes = await contentful.getEntries({
     content_type: 'projectsPage',
   })
-  
+
   const page = pageRes.items[0].fields
 
   const hero: any = page?.hero
@@ -37,21 +33,25 @@ export async function getStaticProps() {
   const { base64: heroBlur } = await getPlaiceholder(heroBuffer)
   const { base64: mobileHeroBlur } = await getPlaiceholder(mobileHeroBuffer)
 
+  // Filter projects without name.
+  // @ts-ignore
+  const projects = page?.projects?.filter((project: any) => project?.fields?.name) || []
+
   return {
     props: {
       hero: {
         altText: hero?.fields?.title,
         blur: heroBlur,
-        url: heroUrl
+        url: heroUrl,
       },
       mobileHero: {
         altText: mobileHero ? mobileHero?.fields?.title : hero?.fields?.title,
         blur: mobileHeroBlur,
-        url: mobileHeroUrl
+        url: mobileHeroUrl,
       },
       pageTitle: page.title,
       text: page.text,
-      bands: page.projects
+      bands: projects,
     },
   }
 }
@@ -61,53 +61,53 @@ const About = ({
   mobileHero,
   pageTitle,
   text,
-  bands
-} : {
-  hero: { altText: string, url: string },
-  mobileHero: { altText: string, url: string },
-  pageTitle: string,
+  bands,
+}: {
+  hero: { altText: string; url: string }
+  mobileHero: { altText: string; url: string }
+  pageTitle: string
   text: string
   bands: any[]
 }) => {
+  console.log('🚀 || bands:', bands)
   return (
-    <Layout transparent={true} pageTitle={pageTitle}>
-        <Hero
-          spaced={false}
-          Image={Image}
-          desktopImg={hero}
-          mobileImg={mobileHero}
-          overlay={true}
-          imagePosition='top'
-          overlayClasses='bg-primary-700 bg-opacity-40 backdrop-blur'
-        >
-          <TextLayout text={text} className='text-white prose-headings:text-white z-10' />
-        </Hero>
+    <Layout
+      transparent={true}
+      pageTitle={pageTitle}
+    >
+      <Hero
+        spaced={false}
+        Image={Image}
+        desktopImg={hero}
+        mobileImg={mobileHero}
+        overlay={true}
+        imagePosition='top'
+        overlayClasses='bg-primary-700 bg-opacity-40 backdrop-blur'
+      >
+        <TextLayout
+          text={text}
+          className='text-white prose-headings:text-white z-10'
+        />
+      </Hero>
       <Container className='flex flex-col justify-center items-center gap-8 lg:gap-16 mb-8 mt-8 lg:mt-16'>
-        {
-          bands.map((band, index) => {
-            return (
-              <>
-                <Band
-                  key={index}
-                  name={band.fields.name}
-                  shortName={band.fields.shortName}
-                  biography={band.fields.text}
-                  image={band.fields.image}
-                  website={band.fields.website}
-                  spotify={band.fields.spotify}
-                  youTube={band.fields.youTube}
-                  facebook={band.fields.facebook}
-                />
-                {
-                  index < bands.length - 1 && (
-                    <div className='h-[1px] w-3/4 bg-primary-500 rounded-full opacity-20'></div>
-                  )
-                
-                }
-              </>
-            )
-          })
-        }
+        {bands.map((band, index) => {
+          return (
+            <>
+              <Band
+                key={index}
+                name={band.fields.name}
+                shortName={band.fields.shortName}
+                biography={band.fields.text}
+                image={band.fields.image}
+                website={band.fields.website}
+                spotify={band.fields.spotify}
+                youTube={band.fields.youTube}
+                facebook={band.fields.facebook}
+              />
+              {index < bands.length - 1 && <div className='h-[1px] w-3/4 bg-primary-500 rounded-full opacity-20'></div>}
+            </>
+          )
+        })}
       </Container>
     </Layout>
   )
